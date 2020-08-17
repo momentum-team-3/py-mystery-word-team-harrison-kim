48c78e076e4ed8c7e21e1a53a3c8e7c84683dd82
Harrison Ballard <harrisonwballard@gmail.com>
2020-08-17 09:33:15 -0400

    more

--- mystery_word.py
+++ mystery_word.py
@@ -4,23 +4,43 @@ guessedLetters = ""
 guessesRemaining = 8
 gameMode = ""
 approvedLetters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+import random
+from math import inf
 
 
+def readDictionary(difficulty):
+    if difficulty == "e":
+        lower, upper = 4, 6
+    elif difficulty == "m":
+        lower, upper = 6, 8
+    else:
+        lower, upper = 8, inf
+    with open("words.txt") as infile:
+        return [word.strip() for word in infile if lower <= len(word.strip()) <= upper]
 
-pickRandomWord("difficulty"):
-    if difficulty == "e"
-        #choose from dictionary word between 4 and 6 letters
-        #set chosenword value to mysteryWord
-    elif difficulty == "m"
-        #choose from dictionary word between 6 and 8 letters
-        #set chosenword value to mysteryWord
-    elif difficulty == "h"
-        #choose from dictionary word that's more than 8 letters
-        #set chosenword value to mysteryWord
 
-guessLetter("inputLetter"):
-    if letter not in approvedLetters:
-        pass
+# difficulty = get_user_difficulty()
+# word = pickRandomWord(difficulty)
+
+
+def pickRandomWord(difficulty):
+    return random.choice(readDictionary(difficulty))
+
+
+def guessLetter():
+    # input_v = False
+    # 
+    # while not input_v:
+    #    user_input = input(...)
+    #    if ... :
+    #        input_v = True
+    #    else:
+    #        print("you screwed it all up!")
+    # 
+    letter = ""
+    while letter not in approvedLetters:
+        print("That is not an approved character.")
+        inputLetter = input("Guess letter: ")
     else:
         letter = inputLetter.lower()
         if letter in mysteryWord:
@@ -30,12 +50,17 @@ guessLetter("inputLetter"):
             guessesRemaining -= 1
             pass
 
+# keep input validation separate; return their guess 
+#then separately test whether in the word
+# 1- is it a letter; 2 - is it in previously guessed letters; 3 - if not, add it to guessed letters 4 - return it 
+# 5 - add to guessed letters 6 - return it 7 - 
 
 
-gameStart():
+def gameStart():
     print("Welcome to Mystery Word!\n")
     print("You can choose between 3 modes of difficulty -- 'e' for easy, 'm' for medium, or 'h' for hard.\n")
     gameMode = input("Enter difficulty level: ")
+    #also need to reset counter to 8 on start
     if gameMode != "e" or "m" or "h"
         raise CustomError ("Error in loading game mode. Please input either 'e', 'm', or 'h'.")
         gameMode()
@@ -45,13 +70,8 @@ gameStart():
         pickRandomWord("m")
     elif gameMode == "h"
         pickRandomWord("h")
+    pass
 
 
-if __name__ == __main:
-    
-
-
-
-
-
-
+if __name__ == "__main__":
+    gameStart()
